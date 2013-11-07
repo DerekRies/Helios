d16ce774f9dac79c3d1e2ed5521a6f44573b1a18
Derek Ries <influenztial@gmail.com>
2013-11-06 21:22:31 -0800

    debouncing

---
+++ app/scripts/services/heliosUtils.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('HeliosApp')
+  .factory('heliosUtils', function () {
+
+    return {
+      parseTimeExpression: function (timeExpression) {
+        // takes a string of some duration of time and converts it to milliseconds
+        // ex:      0.5s -> 500     500ms -> 500     500 -> 500
+        var timeInMs,
+            re = new RegExp(/[0-9]s/),
+            s = timeExpression.search(re);
+        if(s !== -1) {
+          timeInMs = parseFloat(timeExpression, 10) * 1000;
+        }
+        else {
+          timeInMs = parseInt(timeExpression, 10);
+        }
+        return timeInMs;
+      }
+    };
+  });
