702362d673f60ba27ce2a89a1cea1c09040d7b35
nickruiz <nick@fatcatonline.net>
2013-11-06 19:59:01 -0800

    Menu actions

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,22 @@
 'use strict';
 
 angular.module('HeliosApp')
-  .controller('MainCtrl', function ($scope, Hubble) {
+  .controller('MainCtrl', function ($scope) {
 
     $scope.toggleMenu = function () {
       $scope.menuState = !$scope.menuState;
-    }
+    };
+
+    $scope.makeArray = function (x) {
+      console.log(x);
+      var arr = [];
+      for(var i = 0; i <= x; i++) {
+        arr.push(i);
+      }
+      console.log(arr);
+      return arr;
+    };
+
+    $scope.columns = $scope.makeArray(19);
 
   });
